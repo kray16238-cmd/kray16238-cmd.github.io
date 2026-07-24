bec09020608e6c65c0248fffbc987e14b0419c90
kray16238-cmd <kray16238@gmail.com>
2026-07-25 00:55:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,62 @@
+// ===========================
+// script.js
+// ===========================
+
+function searchDrama() {
+
+    let input = document
+        .getElementById("searchInput")
+        .value
+        .toLowerCase();
+
+    let cards = document.querySelectorAll(".movie-card");
+
+    cards.forEach(function(card){
+
+        let title = card.querySelector("h3")
+            .textContent
+            .toLowerCase();
+
+        if(title.includes(input)){
+            card.style.display="block";
+        }else{
+            card.style.display="none";
+        }
+
+    });
+
+}
+
+const buttons=document.querySelectorAll(".movie-card button");
+
+buttons.forEach(function(btn){
+
+    btn.addEventListener("click",function(){
+
+        alert("Add your official streaming link here.");
+
+    });
+
+});
+
+const watchBtn=document.querySelector(".watch-btn");
+
+watchBtn.addEventListener("click",function(){
+
+    document.querySelector(".categories").scrollIntoView({
+        behavior:"smooth"
+    });
+
+});
+
+window.addEventListener("scroll",function(){
+
+    const navbar=document.querySelector(".navbar");
+
+    if(window.scrollY>50){
+        navbar.style.background="#000";
+    }else{
+        navbar.style.background="#111";
+    }
+
+});
